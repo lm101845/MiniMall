2e2d8ab5fc55091b54a279a818e7841b4c4a65dd
lm101845 <1018457683@qq.com>
2021-02-24 00:32:55 +0800

    目录和页面结构划分完成

---
+++ pages/home/home.js
@@ -0,0 +1,15 @@
+// pages/home/home.js
+Page({
+  data: {
+    title:'哈哈哈'
+  },  
+  handlePushDetail(){
+    wx.navigateTo({
+      url: '/pages/detail/detail?title="你好啊"',
+    })
+
+    // wx.redirectTo({
+    //   url: 'url',
+    // })
+  }
+})
